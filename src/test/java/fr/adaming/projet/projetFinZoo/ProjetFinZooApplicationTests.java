04fee0e6da12f7b19d3069b14643414b6b7291f9
G.DeCeglie <horohoro9@hotmail.fr>
2019-12-06 10:23:39 +0100

    projet vierge avec uniquement les dépendances

---
+++ src/test/java/fr/adaming/projet/projetFinZoo/ProjetFinZooApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.adaming.projet.projetFinZoo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetFinZooApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
